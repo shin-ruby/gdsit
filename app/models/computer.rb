d68e391e9becb9dd6e2519a51a9460d64ec5311b
shin <shin.php@gmail.com>
2012-05-28 11:30:25 +0800

    'today_up'

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,2 +1,4 @@
 class Computer < ActiveRecord::Base
+  validates :leixing, :pc, :name, :number, :department, :user, :city, :date, :product,
+            :cpu, :memory, :disk, :mouse, :wired, :sn, :os, :token, :other, :presence => true
 end
